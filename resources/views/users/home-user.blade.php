6a8c4b480f37de9d47d725f09727d9e5cdde9903
Rafael Arraez <rafa.arraez.gue@gmail.com>
2020-07-02 10:39:00 -0400

    new view for user index

--- resources/views/users/home-user.blade.php
+++ resources/views/users/home-user.blade.php
@@ -3,103 +3,98 @@
 @section('content')
 
 <div class="container">
-    <div class="card mb-4">
-        <header class="card-header d-flex align-items-center">
+    <div class="mb-4">
+        <header class="d-flex align-items-center">
             <i class="far fa-list-alt u-sidebar-nav-menu__item-icon"></i>
             <h3 class="h3 card-header-title">Productos Recientes</h3>
         </header>
-
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-hover" id="data-table">
-                    <thead>
-                        <tr>
-                            <th class="text-center" scope="col">#</th>
-                            <th class="text-center" scope="col">Nombre</th>
-                            <th class="text-center" scope="col">Sinonimi</th>
-                            <th class="text-center" scope="col">Ubicaciónn de Salida</th>
-                            <th class="text-center" scope="col">Ubicaciónn de llegada</th>
-						    <th class="text-center" scope="col">Presentación</th>
-                            <th class="text-center" scope="col">COA</th>
-                            <th class="text-center" scope="col">MSDS</th>
-                            <th class="text-center" scope="col">Fecha Limite de reserva</th>
-                            <th class="text-center" scope="col">Fecha Aprox. De Llegada</th>
-                            <th class="text-center" scope="col">Fecha de Llegada</th>
-                            <th class="text-center" scope="col">Cantidad Disponible</th>
-                            <th class="text-center" scope="col">Acciones</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @foreach($products as $product)
-                        @if($product->total_disponible > 0 || $product->total_disponible == null && $product->cantidad_total != $product->total_reservado)
-                                <tr>
-                                    <td class="text-center">{{ $product->id }}</td>
-                                    <td class="text-center">{{ $product->name }}</td>
-                                    <td class="text-center">{{ $product->synonymous }}</td>
-                                    <td class="text-center"><a href="https://www.google.com/maps/place/{{ $product->origin_product }}" target="_blank">{{ $product->origin_product }}</a></td>
-                                    <td class="text-center"><a href="https://www.google.com/maps/place/{{ $product->arrival_location }}" target="_blank">{{ $product->arrival_location }}</a></td>
-						            <td class="text-center">{{ $product->presentation }}</td>
-                                    <td class="text-center"><a href="{{ $product->coa }}" target="_blank">Archivo</a></td>
-                                    <td class="text-center"><a href="{{ $product->msds }}" target="_blank">Archivo</a></td>
-                                    <td class="text-center">{{ Carbon\Carbon::parse($product->deadline)->format('d/m/Y') }}</td>
-                                    <td class="text-center">{{ Carbon\Carbon::parse($product->approximate_date)->format('d/m/Y') }}</td>
-                                    <td class="text-center">{{ Carbon\Carbon::parse($product->arrival_to)->format('d/m/Y') }}</td>
-                                    <td class="text-center">{{ $product->total_disponible == null ? $product->cantidad_total : $product->total_disponible  }}</td>
-                                    <td class="text-center">
-                                        <a class="link-muted" href="#reserve-{{$product->id}}" aria-expanded="false" title="Reservar" data-toggle="modal" data-dismiss="modal" data-backdrop="false">
-                                            Reservar
-                                        </a>
-                                    </td>
-                                    
-                                </tr>
-
-
-                                <!-- Small Size -->
-                                <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" id="reserve-{{$product->id}}">
-                                    <div class="modal-dialog modal-sm" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h3 class="modal-title" id="exampleModalLabel">Desea reservar {{ $product->name }}?</h3>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
+        @foreach($products as $product)
+            @if($product->total_disponible > 0 || $product->total_disponible == null && $product->cantidad_total != $product->total_reservado)
+                <div class="card-body" style="backgroung: #f6f6f6;">
+                    <div class="row">
+                        <div class="col-lg-10 mx-auto">
+                            <!-- List group-->
+                            <ul class="list-group">
+                                <!-- list group item-->
+                                <li class="list-group-item">
+                                    <!-- Custom content-->
+                                    <div class="media align-items-lg-center flex-column flex-lg-row p-3 row">
+                                        <div class="media-body col-lg-6">
+                                            <h3 class="mt-0 font-weight-bold mb-2">{{ $product->name }}</h3>
+                                            <p class="font-italic text-muted mb-0 small">{{ $product->synonymous }} / COA: <a href="{{ $product->coa }}" target="_blank">Archivo</a> / MSDS: <a href="{{ $product->msds }}" target="_blank">Archivo</a></p>
+                                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                                <h5 class="font-weight-bold my-2">Disponibles: <td class="text-center">{{ $product->total_disponible == null ? $product->cantidad_total : $product->total_disponible  }}</h5>
                                             </div>
-                                            <div class="modal-body">
-                                                Por favor confirme la cantidad.
+                                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                                <h5 class="font-weight-bold my-2">Presentación: <td class="text-center">{{ $product->presentation }}</h5>
                                             </div>
-                                            <div class="container">
-                                                @isset( $product)
-                                                <form action="{{ url('/products/reserve/' . Auth::user()->id . '/' . $product->id) }}" method="POST">
-                                                    {{ csrf_field() }}
-                                                    <div class="form-group">
-                                                        <label for="reserve">¿Desea servicio de entraga?:</label>                                                    
-                                                        <input type="checkbox" name="delivery" class="form-control" placeholder="Cantidad a reservar" value="1">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <input type="hidden" id="availible_quantity" name="availible_quantity" value="{{ $product->total_disponible == null ? $product->cantidad_total : $product->total_disponible }}">
-                                                        <label for="reserve">Cantidad:</label>
-                                                        <input type="number" id="reserve_quantity" name="quantity" class="form-control" placeholder="Cantidad a reservar" required>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="submit" class="btn btn-primary">
-                                                            Reservar
-                                                        </button>                                                
-                                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                                                    </div>
-                                                </form>
-                                                @endisset
+                                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                                <h5 class="font-weight-bold my-2">From: <a href="https://www.google.com/maps/place/{{ $product->origin_product }}" target="_blank">{{ $product->origin_product }}</a> -- <span class="text-muted"><i class="fas fa-angle-double-right"></i></span> -- to: <a href="https://www.google.com/maps/place/{{ $product->arrival_location }}" target="_blank">{{ $product->arrival_location }}</a></h5>
                                             </div>
                                         </div>
+                                        <div class="col-lg-6" style="border-left: 1px solid #eaeaea;">
+                                            <div class="d-flex align-items-center justify-content-between">
+                                                <h5 class="font-weight-bold my-2">Fecha maxima de reserva: {{ Carbon\Carbon::parse($product->deadline)->format('d/m/Y') }}</h5>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between">
+                                                <h5 class="font-weight-bold my-2">Fecha aproximada: {{ Carbon\Carbon::parse($product->approximate_date)->format('d/m/Y') }}</h5>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between">
+                                                <h5 class="font-weight-bold my-2">Fecha de llegada: {{ Carbon\Carbon::parse($product->arrival_to)->format('d/m/Y') }}</h5>
+                                            </div>
+
+                                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                                <button class="btn btn-primary" href="#reserve-{{$product->id}}" aria-expanded="false" title="Reservar" data-toggle="modal" data-dismiss="modal" data-backdrop="false" >Reservar</button>
+                                            </div>                            
+                                        </div>
+                                    </div> <!-- End -->
+                                </li> <!-- End -->               
+                            </ul> <!-- End -->
+                        </div>
+                    </div>  
+                </div>
+
+                <!-- Small Size -->
+                <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" id="reserve-{{$product->id}}">
+                    <div class="modal-dialog modal-sm" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h3 class="modal-title" id="exampleModalLabel">Desea reservar {{ $product->name }}?</h3>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Por favor confirme la cantidad.
+                            </div>
+                            <div class="container">
+                                @isset( $product)
+                                <form action="{{ url('/products/reserve/' . Auth::user()->id . '/' . $product->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <div class="form-group">
+                                        <label for="reserve">¿Desea servicio de entraga?:</label>                                                    
+                                        <input type="checkbox" name="delivery" class="form-control" placeholder="Cantidad a reservar" value="1">
                                     </div>
-                                </div>
-                                <!-- End Small Size -->
-                            @endif
-                        @endforeach
-                    </tbody>
-                </table>                
-            </div>
-        </div>        
+                                    <div class="form-group">
+                                        <input type="hidden" id="availible_quantity" name="availible_quantity" value="{{ $product->total_disponible == null ? $product->cantidad_total : $product->total_disponible }}">
+                                        <label for="reserve">Cantidad:</label>
+                                        <input type="number" id="reserve_quantity" name="quantity" class="form-control" placeholder="Cantidad a reservar" required>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">
+                                            Reservar
+                                        </button>                                                
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                                    </div>
+                                </form>
+                                @endisset
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Small Size -->
+            @endif
+        @endforeach     
     </div>    
 </div>
 @endsection
