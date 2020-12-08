4308784c4e872af0bedde673e5960b1506ebb6dd
Tirso Lecointere <tirsolecointere@gmail.com>
2020-12-08 15:53:35 -0400

    update to bootstrap 5 beta

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,12 @@
 
 require('./bootstrap');
 
+var bootstrap = require('bootstrap');
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 // window.Vue = require('vue');
 
 /**
